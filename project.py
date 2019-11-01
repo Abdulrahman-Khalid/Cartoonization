0e1421247cc3dee60fcc65529ae7016bf9ec53f5
Abdulrahman Khalid <abdulrahman.khalid@bk.ru>
2019-11-02 01:24:52 +0200

    shape to numpy array added

--- project.py
+++
@@ -1,9 +0,0 @@
-def rect_to_bb(rect):
-    # take a bounding predicted by dlib and convert it
-    # to the format (x, y, w, h)
-    x = rect.left()
-    y = rect.top()
-    width = rect.right() - x
-    height = rect.bottom() - y
-    # return a tuple of (x, y, w, h)
-    return (x, y, width, height)
