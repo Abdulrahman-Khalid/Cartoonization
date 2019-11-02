2931880407dcb6a21faf950814c1a9ccfe5101bc
Abdulrahman Khalid <abdulrahman.khalid@bk.ru>
2019-11-02 02:34:12 +0200

    many faces detection and number them

--- facial-landmarks/facial_landmarks.py
+++ facial-landmarks/facial_landmarks.py
@@ -1,5 +1,5 @@
 # how to use ?
-# python facial_landmarks.py --shape-predictor shape_predictor.dat --image images/test1.jpg 
+# python facial_landmarks.py --shape-predictor shape_predictor.dat --image images/test1.jpg
 # could detect more than one face at the same image
 
 from imutils import face_utils
@@ -30,3 +30,26 @@ gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 # detect faces in the grayscale image
 rects = detector(gray, 1)
+
+# there are many faces could be detected iterate on them and draw rectangle around them and give them a number
+# loop over the face detections
+for (i, rect) in enumerate(rects):
+    # determine the facial landmarks for the face region, then
+    # convert the facial landmark (x, y)-coordinates to a NumPy
+    # array
+    shape = predictor(gray, rect)
+    shape = face_utils.shape_to_np(shape)
+    # convert dlib's rectangle to a OpenCV-style bounding box
+    # then draw the face bounding box
+    (x, y, w, h) = face_utils.rect_to_bb(rect)
+    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    # show the face number
+    cv2.putText(image, "Face #{}".format(i + 1), (x - 10, y - 10),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+    # loop over the (x, y)-coordinates for the facial landmarks
+    # and draw them on the image
+    for (x, y) in shape:
+        cv2.circle(image, (x, y), 1, (0, 0, 255), -1)
+# show the output image with the face detections + facial landmarks
+cv2.imshow("Output", image)
+cv2.waitKey(0)
