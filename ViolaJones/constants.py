0eee2debd3bdb6c871bb97e5b9223e37ff22d8ea
Abdulrahman Khalid <abdulrahman.khalid@bk.ru>
2019-12-16 04:12:16 +0200

    add violajones for image test folder

---
+++ ViolaJones/constants.py
@@ -0,0 +1,17 @@
+classifiersFileName = 'classifiers.pkl'
+classifiersFileASNumbers = 'classifiers.txt'
+facesTrainingPath = './DataSet/Training/Faces'
+nonFacesTrainingPath = './DataSet/Training/NonFaces'
+facesTestingPath = './DataSet/Testing/Faces'
+nonFacesTestingPath = './DataSet/Testing/NonFaces'
+minFeatureHeight = 3
+minFeatureWidth = 3
+maxFeatureHeight = 18
+maxFeatureWidth = 18
+classifiersNum = 2000
+
+# return rects
+stepSizeW = 50
+stepSizeH = 50
+minSize = 100
+sizeStep = 10
---
+++ ViolaJones/main.py
@@ -0,0 +1,115 @@
+import sys
+import os
+import violajones.AdaBoost as AdaBoost
+import violajones.Utils as utils
+import violajones.IntegralImage as IImg
+import constants as c
+import cv2
+import numpy as np
+
+
+def testClassifiers(classifiers, classifiers_stages, facesTestingPath, nonFacesTestingPath):
+    print('Loading faces testing data set...')
+    facesTesting = utils.load_images(c.facesTestingPath)
+    facesTestingIntegral = list(
+        map(IImg.get_integral_image, facesTesting))
+    print(str(len(facesTesting)) +
+          ' Testing faces data set are loaded!\n')
+    print('Loading testing non faces data set...')
+    nonFacesTesting = utils.load_images(c.nonFacesTestingPath)
+    nonFacesTestingIntegral = list(
+        map(IImg.get_integral_image, nonFacesTesting))
+    print(str(len(nonFacesTesting)) +
+          ' Testing non faces data set are loaded!\n')
+
+    print('Testing selected classifiers ...')
+    correctFacesCount = 0
+    correctNonFacesCount = 0
+
+    # comment this part
+    correctFacesCount = sum(list(len(utils.detect_faces(
+        face, 24, 24, classifiers_stages)) for face in facesTestingIntegral))
+    correctNonFacesCount = len(nonFacesTesting) - sum(list(len(utils.detect_faces(
+        nonFace, 24, 24, classifiers_stages)) for nonFace in nonFacesTestingIntegral))
+
+    # uncomment this part
+    # correctFacesCount = sum(utils.isFaceImgs(
+    #     facesTestingIntegral, classifiers))
+    # correctNonFacesCount = len(
+    #     nonFacesTesting) - sum(utils.isFaceImgs(nonFacesTestingIntegral, classifiers))
+
+    print('Accuracy:-\nFaces: ' + str(correctFacesCount) + '/' + str(len(facesTesting))
+          + '  (' + str((float(correctFacesCount) / len(facesTesting))
+                        * 100) + '%)\nNon Faces: '
+          + str(correctNonFacesCount) + '/' +
+          str(len(nonFacesTesting)) + '  ('
+          + str((float(correctNonFacesCount) / len(nonFacesTesting)) * 100) + '%)')
+
+
+def main():
+    if not os.path.exists(c.classifiersFileName) or (len(sys.argv) >= 2 and sys.argv[len(sys.argv)-1] == 'relearn'):
+        print('Loading faces training data set...')
+        facesTraining = utils.load_images(c.facesTrainingPath)
+        facesTrainingIntegral = list(
+            map(IImg.get_integral_image, facesTraining))
+        print(str(len(facesTraining)) +
+              ' Training faces data set are loaded!\n')
+        print('Loading non faces training data set...')
+        nonFacesTraining = utils.load_images(c.nonFacesTrainingPath)
+        nonFacesTrainingIntegral = list(
+            map(IImg.get_integral_image, nonFacesTraining))
+        print(str(len(nonFacesTraining)) +
+              ' Training non faces data set are loaded!\n')
+
+        classifiers = AdaBoost.adaBoostLearn(facesTrainingIntegral, nonFacesTrainingIntegral,
+                                             c.minFeatureHeight, c.maxFeatureHeight, c.minFeatureWidth, c.maxFeatureWidth, c.classifiersNum)
+
+        utils.save_classifiers(classifiers, c.classifiersFileName)
+        classifiers_stages = utils.classifiers_to_classifiers_stages(
+            classifiers)
+        testClassifiers(classifiers, classifiers_stages,
+                        c.facesTestingPath, c.nonFacesTestingPath)
+
+    elif os.path.exists(c.classifiersFileName) and (len(sys.argv) >= 2 and sys.argv[len(sys.argv)-1] == 'test'):
+        print("Loading Classifiers ...")
+        classifiers = utils.load_classifiers(c.classifiersFileName)
+        print(len(classifiers), "Classifier are loaded")
+        classifiers_stages = utils.classifiers_to_classifiers_stages(
+            classifiers)
+        testClassifiers(classifiers, classifiers_stages,
+                        c.facesTestingPath, c.nonFacesTestingPath)
+        # f = open(c.classifiersFileASNumbers, "w")
+        # for cl in classifiers:
+        #     f.write(str(cl.featureType) + " " + str(cl.topLeft) + " " + str(cl.width) +
+        #             " " + str(cl.height) + " " + str(cl.threshold) + " " + str(cl.polarity) + "\n")
+    elif os.path.exists(c.classifiersFileName):
+        print("Loading Classifiers ...")
+        classifiers = utils.load_classifiers(c.classifiersFileName)
+        print(len(classifiers), "Classifier are loaded")
+        classifiers_stages = utils.classifiers_to_classifiers_stages(
+            classifiers)
+        img = cv2.imread('./test.jpg')
+        frame = cv2.resize(img, (360,360), interpolation = cv2.INTER_AREA)
+        frameGrayScale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        iimage = IImg.get_integral_image(frameGrayScale)
+        frameWidth = frameGrayScale.shape[1]
+        frameHeight = frameGrayScale.shape[0]
+        rects = utils.detect_faces(iimage, frameWidth,
+                                   frameHeight, classifiers_stages)
+        modifiedImage = np.copy(frame)
+        print("rect ",len(rects))
+        # for i in range(len(rects)):
+        #     area = abs(rects[i][0][0]-rects[i][1][0])**2
+        #     h = rects[i][0][0]
+        #     w = rects[i][0][1]
+        #     if(not utils.doubleCheckIsFace([area, h, w], iimage, classifiers_stages)):
+        #         rects.pop(i)
+        for rect in rects:
+            modifiedImage = cv2.rectangle(
+                frame, rect[0], rect[1], (0, 255, 0), 2)
+        cv2.imshow("Test", modifiedImage)
+        cv2.waitKey(0) 
+        cv2.destroyAllWindows()
+
+
+main()
